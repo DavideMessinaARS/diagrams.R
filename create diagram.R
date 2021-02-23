d74193929229ff784f661ad6dd1378fa4939a47c
Davide <davide.messina@ars.toscana.it>
2021-02-23 23:07:47 +0100

    Added xml output and improved script commenting

--- create diagram.R
+++ create diagram.R
@@ -11,12 +11,14 @@ create_id_page <- function() {
 }
 
 # Create new document and root. Number of pages is a variable defined in the parameters.
-create_xml_container <- function(pages, df_tags2){
+create_xml_container <- function(pages){
   test_xml <- xml_new_root("mxfile", host="Electron", modified="2021-02-12T20:24:28.529Z",
                            agent="5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) draw.io/14.1.8 Chrome/87.0.4280.88 Electron/11.1.1 Safari/537.36",
                            etag="fHMIuIajccZ_3DrzuGlE", version="14.1.8", type="device", pages = as.character(1))
   id_page <- create_id_page()
   vect_vars_diagram <- c("id", "name")
+  
+  df_tags2 <- create_vector_param_page()
 
   for (page in 1:pages) {
     
@@ -33,7 +35,13 @@ create_xml_container <- function(pages, df_tags2){
   return(test_xml)
 }
 
-## parameters
+
+## ##%######################################################%##
+#                                                          #
+####                     parameters                     ####
+#                                                          #
+##%######################################################%##
+
 
 # create styles and default values in a dataset (some parameter might be eliminated altogether, for example arrow width)
 create_page_styles_df <- function() {
@@ -44,10 +52,18 @@ create_page_styles_df <- function() {
   )
 }
 
-page_styles <- create_page_styles_df()
 
-create_vector_param_page <- function(df_styles) {
-  temp_df <- df_styles %>%
+## ##%######################################################%##
+#                                                          #
+####                auxiliary functions                 ####
+#                                                          #
+##%######################################################%##
+
+# Call the creation of the df containing the styles of pages,
+# take the default (for now), exclude the name of the style
+# transform the output to named character vector
+create_vector_param_page <- function() {
+  temp_df <- create_page_styles_df() %>%
     filter(name_style == "default") %>%
     select(-name_style)
   temp_vect <- as.character(as.vector(temp_df))
@@ -55,11 +71,46 @@ create_vector_param_page <- function(df_styles) {
   return(temp_vect)
 }
 
-test_xml <- create_xml_container(1, create_vector_param_page(page_styles))
+# Create new document and root. Number of pages is a variable defined in the parameters.
+# TODO support for deciding styles
+# TODO support for different style for each page
+create_xml_container <- function(pages){
+  test_xml <- xml_new_root("mxfile", host="Electron", modified="2021-02-12T20:24:28.529Z",
+                           agent="5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) draw.io/14.1.8 Chrome/87.0.4280.88 Electron/11.1.1 Safari/537.36",
+                           etag="fHMIuIajccZ_3DrzuGlE", version="14.1.8", type="device", pages = as.character(1))
+  id_page <- create_id_page()
+  vect_vars_diagram <- c("id", "name")
+  
+  df_tags2 <- create_vector_param_page()
+  
+  for (page in 1:pages) {
+    
+    xml_add_child(test_xml, "diagram")
+    vect_values_diagram <- c(id_page[page], paste0("Page-", page))
+    names(vect_values_diagram) <- vect_vars_diagram
+    xml_set_attrs(xml_children(test_xml)[length(xml_children(test_xml))], vect_values_diagram)
+    
+    xml_add_child(xml_children(test_xml), "mxGraphModel")
+    xml_set_attrs(xml_children(xml_children(test_xml))[length(xml_children(xml_children(test_xml)))], df_tags2)
+    xml_add_child(xml_children(xml_children(test_xml)), "root")
+    
+  }
+  return(test_xml)
+}
+
+
+##%######################################################%##
+#                                                          #
+####                   script to run                    ####
+#                                                          #
+##%######################################################%##
+
+
+test_xml <- create_xml_container(2)
 
 # create a dataset with nodes/arrows relations (or two distinct dfs)
 # create a second dataset for the order of "layers" or create the one with relationships map already ordered
 
 # create a dataset containing links, tags, tooltip and other attributes (placeholders, shape, ...)
 
-write_xml(test_xml, "C:\\Users\\Davide\\Downloads\\test r.xml")
+write_xml(test_xml, "test r.xml")

5f9bee86aa0a0a23f79a1496476eac24a643eaec
Davide Messina <davide.messina@ars.toscana.it>
2022-07-25 21:01:25 +0200

    better draft

--- convert conception to roel.R
+++ convert conception to roel.R
@@ -1,5 +1,8 @@
 library(readr)
 library(readxl)
+library(magrittr)
+library(dplyr)
+library(tidyr)
 
 test <- readr::read_csv2("Program.csv",
                          locale = readr::locale(decimal_mark = ",", grouping_mark = "."),
@@ -8,8 +11,14 @@ test <- readr::read_csv2("Program.csv",
 
 
 test1 <- readxl::read_excel("index.xlsx")
-test1 <- test1 %>% select(dataset_name, step_producing_this_dataset, folder_where_the_datset_is_stored,
-                          input_datasets_for_the_step) %>%
-  
+test2 <- test1 %>% select(dataset_name, step_producing_this_dataset, folder_where_the_datset_is_stored) %>%
+  mutate(TYPE = "OUTPUT") %>%
+  unique()
 
-test1 <- test1(TYPE)
+test3 <- test1 %>% select(step_producing_this_dataset, folder_where_the_datset_is_stored,
+                          dataset_name = input_datasets_for_the_step) %>%
+  drop_na(dataset_name) %>%
+  mutate(TYPE = "INPUT") %>%
+  unique()
+
+test23 <- rbind(test2, test3)

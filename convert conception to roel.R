ff1b6f69e5dac1dc014200662613995ac5dc53d6
Davide <davide.messina@ars.toscana.it>
2022-10-10 23:23:04 +0200

    convert conception index to roel index

--- convert conception to roel.R
+++ convert conception to roel.R
@@ -3,22 +3,46 @@ library(readxl)
 library(magrittr)
 library(dplyr)
 library(tidyr)
+library(stringr)
 
-test <- readr::read_csv2("Program.csv",
-                         locale = readr::locale(decimal_mark = ",", grouping_mark = "."),
-                         show_col_types = FALSE) %>%
-  select(PROGRAM, FOLDER_VAR, FILE, TYPE)
+conception_to_roel <- function(file) {
+  
+  test1 <- file
+  test2 <- test1 %>% select(dataset_name, step_producing_this_dataset, folder_where_the_datset_is_stored) %>%
+    mutate(TYPE = "OUTPUT") %>%
+    unique()
+  
+  test3 <- test1 %>% select(step_producing_this_dataset, folder_where_the_datset_is_stored,
+                            dataset_name = input_datasets_for_the_step) %>%
+    drop_na(dataset_name) %>%
+    mutate(TYPE = "INPUT") %>%
+    unique() %>%
+    separate_rows(dataset_name, sep = " ", convert = FALSE)
+  
+  test23 <- rbind(test2, test3)
+  test23 <- test23 %>%
+    mutate(folder_where_the_datset_is_stored = "a") %>%
+    rename(PROGRAM = "step_producing_this_dataset") %>%
+    rename(FOLDER_VAR = "folder_where_the_datset_is_stored") %>%
+    rename(FILE = "dataset_name") %>%
+    select(PROGRAM, FOLDER_VAR, FILE, TYPE)
+  
+  return(test23)
+}
 
 
-test1 <- readxl::read_excel("index.xlsx")
-test2 <- test1 %>% select(dataset_name, step_producing_this_dataset, folder_where_the_datset_is_stored) %>%
-  mutate(TYPE = "OUTPUT") %>%
-  unique()
 
-test3 <- test1 %>% select(step_producing_this_dataset, folder_where_the_datset_is_stored,
-                          dataset_name = input_datasets_for_the_step) %>%
-  drop_na(dataset_name) %>%
-  mutate(TYPE = "INPUT") %>%
-  unique()
 
-test23 <- rbind(test2, test3)
+
+data.table::fwrite(test23, "index.csv")
+
+source("create diagram.R")
+source("helper_functions.R")
+source("styles_and_parameters.R")
+
+steps_style <- c(cell_style = "circle")
+datamodels_style <- c(cell_style = "yellow")
+arrows_style <- c(arrow_style = "circle arrow")
+
+test_xml <- create_diagram("index.csv", pages = 1, arrows_style, steps_style, datamodels_style, direction = "TB")
+xml2::write_xml(test_xml, "test CovE.xml")

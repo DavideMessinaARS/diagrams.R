5e376994256505f6080ae64f2f1d0698f4b0f6e1
Davide <davide.messina@ars.toscana.it>
2022-10-10 23:24:29 +0200

    added conception to roel

--- create diagram.R
+++ create diagram.R
@@ -2,6 +2,35 @@ library(xml2)
 library(tibble)
 library(dplyr)
 library(magrittr)
+library(readr)
+library(readxl)
+library(tidyr)
+library(stringr)
+
+conception_to_roel <- function(file) {
+  
+  test1 <- file
+  test2 <- test1 %>% select(dataset_name, step_producing_this_dataset, folder_where_the_datset_is_stored) %>%
+    mutate(TYPE = "OUTPUT") %>%
+    unique()
+  
+  test3 <- test1 %>% select(step_producing_this_dataset, folder_where_the_datset_is_stored,
+                            dataset_name = input_datasets_for_the_step) %>%
+    drop_na(dataset_name) %>%
+    mutate(TYPE = "INPUT") %>%
+    unique() %>%
+    separate_rows(dataset_name, sep = " ", convert = FALSE)
+  
+  test23 <- rbind(test2, test3)
+  test23 <- test23 %>%
+    mutate(folder_where_the_datset_is_stored = "a") %>%
+    rename(PROGRAM = "step_producing_this_dataset") %>%
+    rename(FOLDER_VAR = "folder_where_the_datset_is_stored") %>%
+    rename(FILE = "dataset_name") %>%
+    select(PROGRAM, FOLDER_VAR, FILE, TYPE)
+  
+  return(test23)
+}
 
 ##%######################################################%##
 #                                                          #
